af91ced32cbe71a1a250d8c5f0a325c5f376d7a8
HarshitCoder437 <70216081+HarshitCoder437@users.noreply.github.com>
2020-12-21 18:19:19 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,19 @@
+class Dustbin {
+    constructor(x,y,width,height){
+        var options = {
+            isStatic : true
+        };
+
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.body = Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        fill(255,255,255);
+        rect(pos.x,pos.y,this.width,this.height);
+    } 
+}
